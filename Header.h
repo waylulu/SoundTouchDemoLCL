fe098b61a4cc7a7d084ba101a1e80106455d3340
waylulu <924686691@qq.com>
2018-02-07 17:45:53 +0800

    AudioConvert
    文件一

---
+++ Header.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//  LvSoundTouch
+//
+//  Created by lv on 2018/2/5.
+//  Copyright © 2018年 ss. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import "Recorder.h"
+#import "SVProgressHUD.h"
+#import "AudioConvert.h"
+#import "DotimeManage.h"
+
+#endif /* Header_h */
